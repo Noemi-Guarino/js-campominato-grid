a1da20e6878e22be5bb58482517ce935147ce38a
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-13 19:43:53 +0100

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,25 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-let containercell = document.querySelector(".containercell");
+// seleziono il contenitore
+let containercell = document.querySelector(".containercell"); 
 
+// agg la funzione al bottone
     let btnGenerate = document.getElementById("generate");
     btnGenerate.addEventListener ("click", 
         function (){
-
+            // creo un ciclo for per generare i numeri da 1/100
             for (let i = 0; i < 100 + 1; i++){
                 console.log(i);
+                // creo un div nel conteiner e gli agg una classe
                 let div = document.createElement('div');
                 div.classList.add('cell');
                 containercell.append(div);
                 div.innerHTML = i;
 
-                div.addEventListener ("click", 
+// creo un avvenimento cosi che cliccando sulla cella la colora di blu (aggiungendo la classe che andrò a stilizzare nel css nel caso in cui ci sia bisogno)
+
+                div.addEventListener("click", 
                 function (){
 
                     // let cellcolor = document.getElementsByClassName("cell");
